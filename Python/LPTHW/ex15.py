9f77c6e09a39af7ea4af0733055959141c76e4f2
Kevin Liu <kevin07liu@gmail.com>
2014-12-09 23:05:41 -0700

    Make some change of ex15

--- Python/LPTHW/ex15.py
+++ Python/LPTHW/ex15.py
@@ -1,11 +1,11 @@
-from sys import argv
+from sys import argv #Import module from labriary
 
-script, filename = argv
+script, filename = argv #To make varible to argument varibles
 
-txt = open(filename)
+txt = open(filename) #Give the txt varible the file contents
 
-print "Here's your file %r:" % filename
-print txt.read()
+print "Here's your file %r:" % filename #Give a prompt in English
+print txt.read() #Use .read() method to read file contents
 
 print "Type the filename again:"
 file_again = raw_input("> ")
