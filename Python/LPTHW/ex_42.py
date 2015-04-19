b240de575dafb830dcab195d0c5a9c1f667308ed
Kevin Liu <kevin07liu@gmail.com>
2015-04-18 17:04:48 -0700

    Modify the ex_42.py a little bit

--- Python/LPTHW/ex_42.py
+++ Python/LPTHW/ex_42.py
@@ -28,44 +28,44 @@ class Employee(Person):
     def __init__(self, name, salary):
         ##  hmm what is this sxtrange magic?
         super(Employee, self).__init__(name)
-        ## ??
+        ## has-a
         self.salary = salary
 
-## ??
+## is-a
 class Fish(object):
     pass
 
-## ??
+## is-a
 class Salmon(Fish):
     pass
 
-##??
+## is-a
 class Halibut(Fish):
     pass
 
 ## rover is a Dog
 rover = Dog("Rover")
 
-## ??
+## is-a
 satan = Cat("Satan")
 
-## ??
+## is-a
 mary = Person("Mary")
 
-## ??
+## has-a
 mary.pet = satan
 
-## ??
+## is-a
 frank = Employee("Frank", 120000)
 
-## ??
+## has-a
 frank.pet = rover
 
-## ??
+## is-a
 flipper = Fish()
 
-## ??
+## is-a
 crouse = Salmon()
 
-## ??
+## is-a
 harry = Halibut()
